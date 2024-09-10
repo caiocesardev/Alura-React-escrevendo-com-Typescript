c0b2fb60ffb47cc61a57c6744673235f3f528a98
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-09-10 20:07:56 -0300

    01.06.05 - Mostrando a tarefa selecionada

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -16,13 +16,14 @@ export default function Item(
   }: Props) {
   console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
   return (
-    <li className={style.item} onClick={() => selecionaTarefa({
-      tarefa,
-      tempo,
-      selecionado,
-      completado,
-      id
-    })}>
+    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      onClick={() => selecionaTarefa({
+        tarefa,
+        tempo,
+        selecionado,
+        completado,
+        id
+      })}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
