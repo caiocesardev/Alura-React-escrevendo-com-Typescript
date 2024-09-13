d471d2686c2f827df90ca781632049d0fe1d1eed
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-09-13 20:19:23 -0300

    01.07.08 - Finalizando a aplicação

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -15,8 +15,10 @@ export default function Item(
     selecionaTarefa
   }: Props) {
   return (
-    <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-      onClick={() => selecionaTarefa({
+    <li className={`${style.item} ${selecionado ? style.
+      itemSelecionado : ''} ${completado ? style.
+        itemCompletado : ''}`}
+      onClick={() => !completado && selecionaTarefa({
         tarefa,
         tempo,
         selecionado,
@@ -25,6 +27,7 @@ export default function Item(
       })}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && <span className={style.concluido} aria-label="tarefa completada"></span>}
     </li>
   )
 }
